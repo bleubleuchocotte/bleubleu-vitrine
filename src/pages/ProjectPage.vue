b0fdd65b156b2a07980b1f2fa151d3caea09ea86
Hashs7 <seb_her@hotmail.fr>
2021-10-29 00:22:01 +0200

    Refacto: query by type

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-link to="/">Home</router-link>
+  <router-link to="/">
+    Home
+  </router-link>
   <div v-if="projects">
     <ProjectsList :projects="projects" />
   </div>
@@ -10,14 +12,11 @@ import { usePrismic } from '@prismicio/vue';
 import { ref } from 'vue';
 import ProjectsList from '../components/ProjectsList.vue';
 
-const { client, predicate } = usePrismic();
+const { client } = usePrismic();
 let projects = ref(null);
 
 async function getContent() {
-  const allDocuments = await client.query('');
-  // Temporary fix for fetching with query
-  projects.value = allDocuments.results.filter(doc => doc.type === 'project');
-  // response.value = await client.query([ predicate.at('document.type', 'project') ], {});
+  projects.value = (await client.getByType('project')).results;
   console.log(projects.value);
 }
 
