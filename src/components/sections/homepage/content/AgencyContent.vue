b3bb64f142b0d44e52005c2e96809c25bf61206a
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-09-28 16:48:10 +0200

    create component

---
+++ src/components/sections/homepage/content/AgencyContent.vue
@@ -0,0 +1,44 @@
+<script setup>
+	import { defineProps } from 'vue';
+import ScrollMovingText from '../../../UI/ScrollMovingText.vue';
+
+	defineProps({
+		properties: {
+			type: Array,
+			required: true,
+		}
+	})
+</script>
+
+<template>
+  <section>
+    <div class="border" />
+    <ScrollMovingText
+      v-for="(property, index) in properties"
+      :key="index"
+      :text="property.text"
+      :direction="property.direction"
+      :speed="property.speed"
+      :left="property.position.left"
+      :top="property.position.top"
+      :z-index="property.zIndex"
+    />
+  </section>
+</template>
+
+<style scoped lang="scss">
+	section{
+		height: 3500px;
+		padding: 0 $global-horizontal-padding 0 $global-horizontal-padding;
+		border-bottom: 1px solid $green;
+		position: relative;
+	}
+
+	.border{
+		width: 100%;
+		height: 100%;
+
+		border-left: 1px solid $green;
+		border-right: 1px solid $green;
+	}
+</style>
