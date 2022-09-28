4b69ae2f9f56e78b260b43de404b7d1ed152eeec
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-09-28 15:38:38 +0200

    add homepageAfterHeader component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,25 +4,26 @@
     class="home-page"
   >
     <HeaderNewDesign :socials="document.data.socials" />
-    <HomepageEntrySection :socials="document.data.socials" />
+    <HomepageAfterHeaderSection :fields="document.data" />
+
+    <!-- <HomepageEntrySection :socials="document.data.socials" />
     <HomepageContentSection :fields="document.data" />
-    <HomepageTeamSection :members="document.data.team_members" />
+    <HomepageTeamSection :members="document.data.team_members" /> -->
     <Footer />
   </div>
 
-  <!-- <span class="line-top o-grid-item--full-top" /> -->
   <span class="line-bottom o-grid-item--full-bottom" />
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import { usePrismic } from '@prismicio/vue';
-// import Header from '../components/layout/Header.vue'
 import HomepageEntrySection from '../components/sections/homepage/HomepageEntrySection.vue';
 import HomepageTeamSection from '../components/sections/homepage/HomepageTeamSection.vue';
 import Footer from '../components/layout/Footer.vue';
 import HomepageContentSection from '../components/sections/homepage/HomepageContentSection.vue';
 import HeaderNewDesign from '../components/layout/HeaderNewDesign.vue';
+import HomepageAfterHeaderSection from '../components/sections/homepage/HomepageAfterHeaderSection.vue';
 
 const { client } = usePrismic();
 let document = ref(null);
