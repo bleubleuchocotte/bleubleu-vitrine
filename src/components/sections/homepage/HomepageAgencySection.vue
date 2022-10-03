eae78eaf67564eb26ec648e2b9ba0231210d538e
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-03 09:55:21 +0200

    change single styling property with an object

--- src/components/sections/homepage/HomepageAgencySection.vue
+++ src/components/sections/homepage/HomepageAgencySection.vue
@@ -6,44 +6,56 @@ const property = ref([
 	{
 		text: "sound design",
 		direction: "ltr",
-		position: {left: "50%", top: "34px"},
-		zIndex: 3,
-		speed: 1
+		styles: {
+			left: "0%",
+			top: "34px",
+			zIndex: 3,
+		},
 	},
 	{
 		text: "web dev",
 		direction: "ltr",
-		position: {left: "50%", top: "105px"},
-		zIndex: 1,
-		speed: 1
+		styles: {
+			left: "0%",
+			top: "34px",
+			zIndex: 3,
+		},
 	},
 	{
 		text: "motion design",
 		direction: "rtl",
-		position: {left: "50%", top: "136px"},
-		zIndex: 2,
-		speed: 1
+		styles: {
+			left: "0%",
+			top: "34px",
+			zIndex: 3,
+		},
 	},
 	{
 		text: "brand identity",
 		direction: "ltr",
-		position: {left: "50%", top: "195px"},
-		zIndex: 2,
-		speed: 1
+		styles: {
+			left: "0%",
+			top: "34px",
+			zIndex: 3,
+		},
 	},
 	{
 		text: "ux/ui design",
 		direction: "rtl",
-		position: {left: "50%", top: "237px"},
-		zIndex: 2,
-		speed: 1
+		styles: {
+			left: "0%",
+			top: "34px",
+			zIndex: 3,
+		},
 	},
 	{
 		text: "graphic design",
 		direction: "rtl",
-		position: {left: "50%", top: "64px"},
-		zIndex: 2,
-		speed: 1
+		styles: {
+			left: "0%",
+			top: "34px",
+			zIndex: 3,
+		},
 	}
 ])
 </script>
