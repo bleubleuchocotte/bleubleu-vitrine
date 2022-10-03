7e20614ff3c4635fab6b1220f78ab5ab4151f9a2
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-03 10:25:54 +0200

    move intersection observer there to trigger on section

--- src/components/sections/homepage/content/AgencyContent.vue
+++ src/components/sections/homepage/content/AgencyContent.vue
@@ -1,32 +1,56 @@
 <script setup>
-	import { defineProps } from 'vue';
+import { defineProps, ref, onMounted } from 'vue';
 import ScrollMovingText from '../../../UI/ScrollMovingText.vue';
 
-	defineProps({
-		properties: {
-			type: Array,
-			required: true,
-		}
-	})
+defineProps({
+	properties: {
+		type: Array,
+		required: true,
+	}
+})
+
+onMounted(() => {
+  createIntersectionObserver();
+});
+
+function createIntersectionObserver() {
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  let callback = (entries) => {
+    console.log(entries);
+    entries[0].isIntersecting
+      ? (isInView.value = true)
+      : (isInView.value = false);
+  };
+  let observer = new IntersectionObserver(callback, options);
+  observer.observe(sectionHTML.value);
+}
+
+const sectionHTML = ref(null);
+const isInView = ref(false);
 </script>
 
 <template>
-  <section>
+  <section ref="sectionHTML">
     <div class="border" />
     <ScrollMovingText
       v-for="(property, index) in properties"
       :key="index"
       :text="property.text"
-      :direction="property.direction"
       :speed="property.speed"
       :styles="property.styles"
+      :is-in-view="isInView"
     />
   </section>
 </template>
 
 <style scoped lang="scss">
 	section{
-		height: 3500px;
+		height: 350px;
 		padding: 0 $global-horizontal-padding 0 $global-horizontal-padding;
 		border-bottom: 1px solid $green;
 		position: relative;
