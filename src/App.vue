68930b2ce0a6d32644ed662ec158a0dc8c6e450c
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-04 17:00:32 +0200

    random color picker

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
+<script setup>
+import { onMounted } from 'vue';
+
+	const colors = [
+		{
+			name: "Darker",
+			primary: "#000000",
+			secondary: "#FFFFFF"
+		},
+		{
+			name: "Passion",
+			primary: "#FF0000",
+			secondary: "#FFFFFF"
+		},
+		{
+			name: "Jelly",
+			primary: "#BBFF65",
+			secondary: "#969696"
+		}
+	]
+
+	onMounted(() => {
+		const randomColor = colors[getRandomInteger(colors.length)];
+		document.querySelector(':root').style.setProperty('--primary-color', randomColor.primary);
+		document.querySelector(':root').style.setProperty('--secondary-color', randomColor.secondary);
+		console.log(randomColor.name);
+
+	})
+
+
+
+	function getRandomInteger(max){
+		return Math.floor(Math.random() * max);
+	}
+</script>
+
 <template>
-  <!-- <GradiantFrame /> -->
   <router-view />
 </template>
 
-<script setup>
-// import GradiantFrame from './components/layout/GradiantFrame.vue';
-</script>
