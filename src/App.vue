7689ba39ce78e2815f90b3a4bffbbe3a5d3bae99
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-05 14:21:55 +0200

    add filter parameter to each color

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@ import { onMounted } from 'vue';
 		{
 			name: "Darker",
 			primary: "#000000",
-			secondary: "#FFFFFF"
+			secondary: "#FFFFFF",
+			filter: "none"
 		},
 		{
 			name: "Passion",
 			primary: "#FF0000",
-			secondary: "#FFFFFF"
+			secondary: "#FFFFFF",
+			filter: "invert(19%) sepia(77%) saturate(6759%) hue-rotate(359deg) brightness(91%) contrast(124%)"
 		},
 		{
 			name: "Jelly",
 			primary: "#BBFF65",
-			secondary: "#969696"
+			secondary: "#969696",
+			filter: "invert(81%) sepia(17%) saturate(1281%) hue-rotate(40deg) brightness(111%) contrast(101%)"
 		}
 	]
 
@@ -23,6 +26,7 @@ import { onMounted } from 'vue';
 		const randomColor = colors[getRandomInteger(colors.length)];
 		document.querySelector(':root').style.setProperty('--primary-color', randomColor.primary);
 		document.querySelector(':root').style.setProperty('--secondary-color', randomColor.secondary);
+		document.querySelector(':root').style.setProperty('--filter-to-primary', randomColor.filter);
 		console.log(randomColor.name);
 
 	})
