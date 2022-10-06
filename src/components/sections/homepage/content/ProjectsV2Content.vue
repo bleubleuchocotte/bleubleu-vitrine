8807978d030dd37ba5ea01041b532b2490140b82
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-06 16:21:58 +0200

    create file

---
+++ src/components/sections/homepage/content/ProjectsV2Content.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+import { defineProps } from 'vue';
+
+const props = defineProps({
+fields: {
+	type: Array,
+	required: true,
+}
+
+})
+console.log(props.fields);
+
+</script>
+
+<template>
+  <!-- <div
+    v-for="(project, index) in props.fields"
+    :key="index"
+  >
+    <pre>{{ project.data }}</pre>
+  </div> -->
+
+
+  <article>
+    <p>popcorn x bleubleu</p>
+    <p>2022</p>
+  </article>
+</template>
+
+<style scoped lang="scss"></style>
