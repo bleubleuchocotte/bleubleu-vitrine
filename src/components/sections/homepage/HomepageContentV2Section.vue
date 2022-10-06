3b04c508fd00074491cf8d6d6260407f1af6eb24
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-06 16:21:27 +0200

    create file

---
+++ src/components/sections/homepage/HomepageContentV2Section.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <ProjectsV2Content :fields="fields" />
+  </section>
+</template>
+  
+<script setup>
+import { defineProps } from 'vue';
+import ProjectsV2Content from './content/ProjectsV2Content.vue';
+  
+defineProps({
+	fields: {
+		type: Array,
+		required: true,
+	}
+})
+</script>
+
+<style lang="scss">
+
+</style>
