ab9494e5bf0fe56799307f9620040b904e65c2b4
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-06 17:03:48 +0200

    make more divs to separate content

--- src/components/sections/homepage/content/ProjectsV2Content.vue
+++ src/components/sections/homepage/content/ProjectsV2Content.vue
@@ -23,55 +23,71 @@ console.log(props.fields);
 
 
   <article>
-    <div class="article__project-title">
-      <p>
-        popcorn x bleubleu
-      </p>
+    <div class="article__top">
+      <div class="article__project-title">
+        <p>
+          popcorn x bleubleu
+        </p>
+      </div>
+      <div class="article__project-date">
+        <p>2022</p>
+      </div>
     </div>
-    <div class="article__project-date">
-      <p>2022</p>
-    </div>
+    <div class="article__middle" />
   </article>
 </template>
 
 <style scoped lang="scss">
 
-	article{
-		display: flex;
-		width: 100%;
+article{
+	width: 100%;
+	height: 75px;
 
-		border-left: 1px solid $primary;
-		border-right: 1px solid $primary;
+	transition: height 0.5s ease-out;
 
-		.article__project-title{
-			padding: 13px 20px;
-			flex: 0.35;
+	border-left: 1px solid $primary;
+	border-right: 1px solid $primary;
 
-			border-right: 1px solid $primary;
-			
-			p{
-				width: fit-content;
-				padding: 10px 20px;
+	&:hover{
+		height: 500px;
+	}
+
+	&:not(:first-of-type){
+		border-top: 1px solid $primary;
+	}
 	
-				border: 1px solid $primary;
-				border-radius: 58px;
-			}
-		}
+}
+.article__top{
+	display: flex;
+	height: 75px;
+}
 
-		.article__project-date{
-			flex: 0.1;
-			display: flex;
-			align-items: center;
-			padding: 13px 20px;
-			border-right: 1px solid $primary;
-			
-		}
+.article__project-date{
+		flex: 0.1;
+		display: flex;
+		align-items: center;
+		padding: 13px 20px;
+		border-right: 1px solid $primary;
 		
-	}
+}
+.article__project-title{
+	padding: 13px 20px;
+	flex: 0.35;
 
-	p {
-		font-size: 20px;
-		text-transform: uppercase;
+	border-right: 1px solid $primary;
+	
+	p{
+		width: fit-content;
+		padding: 10px 20px;
+
+		border: 1px solid $primary;
+		border-radius: 58px;
 	}
+}
+
+p {
+	font-size: 20px;
+	text-transform: uppercase;
+}
 
 </style>
