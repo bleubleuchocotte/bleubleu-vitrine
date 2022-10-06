7b6bd14d9f57026f39b78733edf6aa97a531543a
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-06 16:44:48 +0200

    add html to diplsay one line

--- src/components/sections/homepage/content/ProjectsV2Content.vue
+++ src/components/sections/homepage/content/ProjectsV2Content.vue
@@ -23,9 +23,55 @@ console.log(props.fields);
 
 
   <article>
-    <p>popcorn x bleubleu</p>
-    <p>2022</p>
+    <div class="article__project-title">
+      <p>
+        popcorn x bleubleu
+      </p>
+    </div>
+    <div class="article__project-date">
+      <p>2022</p>
+    </div>
   </article>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+	article{
+		display: flex;
+		width: 100%;
+
+		border-left: 1px solid $primary;
+		border-right: 1px solid $primary;
+
+		.article__project-title{
+			padding: 13px 20px;
+			flex: 0.35;
+
+			border-right: 1px solid $primary;
+			
+			p{
+				width: fit-content;
+				padding: 10px 20px;
+	
+				border: 1px solid $primary;
+				border-radius: 58px;
+			}
+		}
+
+		.article__project-date{
+			flex: 0.1;
+			display: flex;
+			align-items: center;
+			padding: 13px 20px;
+			border-right: 1px solid $primary;
+			
+		}
+		
+	}
+
+	p {
+		font-size: 20px;
+		text-transform: uppercase;
+	}
+
+</style>
