84075a61d5de850d68c0dad608ea26958965f4b3
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-12 15:08:18 +0200

    create component

---
+++ src/components/sections/homepage/HomepageTeamSection.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TeamContent from "@/components/sections/homepage/content/TeamContent.vue";
+import { defineProps } from "vue";
+
+defineProps({
+	members: {
+		type: Array,
+		required: true
+	}
+})
+
+</script>
+
+<template>
+  <section id="team">
+    <TeamContent :members="members" />
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+	section {
+		margin: 180px 0;
+		padding: 0 $global-horizontal-padding 0 $global-horizontal-padding;
+	}
+
+</style>
