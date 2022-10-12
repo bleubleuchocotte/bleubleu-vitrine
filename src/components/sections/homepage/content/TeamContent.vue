56c63f02d5896786c061103b728d9a19a4d10480
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-12 15:08:11 +0200

    create component

---
+++ src/components/sections/homepage/content/TeamContent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+	members: {
+		type: Array,
+		required: true
+	}
+})
+</script>
+
+<template>
+  <a
+    v-for="(member, index) in members"
+    :key="index"
+    :href="member.link.url"
+    :target="member.link.target"
+  > 
+    {{ member.name }} 
+  </a>
+</template>
+
+<style scoped lang="scss">
+
+a {
+	display: block;
+	text-align: center;
+	font-size: 42px;
+
+	min-width: 510px;
+	width: fit-content;
+
+	margin: 0 auto;
+	padding: 0.5rem 2rem;
+
+	border: 1px solid $primary;
+}
+
+</style>
