66ad7f0025b39f80e2d8a7c5c1539c90337ae245
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-13 15:07:29 +0200

    remove uselss comment

--- src/components/sections/homepage/HomepageAgencySection.vue
+++ src/components/sections/homepage/HomepageAgencySection.vue
@@ -14,67 +14,6 @@ defineProps({
 	}
 })
 
-// const property = ref([
-// 	{
-// 		text: "sound design",
-// 		direction: "rtl",
-// 		styles: {
-// 			right: "0",
-// 			top: "34px",
-// 			zIndex: 3,
-// 		},
-// 		speed: 1
-// 	},
-// 	{
-// 		text: "web dev",
-// 		direction: "ltr",
-// 		styles: {
-// 			left: "0%",
-// 			top: "105px",
-// 			zIndex: 3,
-// 		},
-// 	},
-// 	{
-// 		text: "motion design",
-// 		direction: "ltr",
-// 		styles: {
-// 			left: "0%",
-// 			top: "136px",
-// 			zIndex: 3,
-// 		},
-// 		speed: 1.25
-// 	},
-// 	{
-// 		text: "brand identity",
-// 		direction: "rtf",
-// 		styles: {
-// 			left: "10%",
-// 			top: "195px",
-// 			zIndex: 3,
-// 		},
-// 		speed: 0.15
-// 	},
-// 	{
-// 		text: "ux/ui design",
-// 		direction: "ltr",
-// 		styles: {
-// 			left: "0%",
-// 			top: "237px",
-// 			zIndex: 3,
-// 		},
-// 		speed: 1
-// 	},
-// 	{
-// 		text: "graphic design",
-// 		direction: "ltr",
-// 		styles: {
-// 			left: "0%",
-// 			top: "64px",
-// 			zIndex: 3,
-// 		},
-// 		speed: 0.75
-// 	}
-// ])
 </script>
 
 <template>
