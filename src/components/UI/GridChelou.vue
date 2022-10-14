cce1d4b0732ff51b2bfbed2a17ac4f614863b2c0
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-14 15:52:37 +0200

    add auto animation on image

--- src/components/UI/GridChelou.vue
+++ src/components/UI/GridChelou.vue
@@ -40,6 +40,19 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 
 	}
 
+	.left:hover:has( > div:first-of-type:hover){
+		flex: 0.5;
+	}
+	.left:hover:has( > div:last-of-type:hover){
+		flex: 0.7;
+	}
+	.right:hover:has( > div:first-of-type:hover){
+		flex: 0.6;
+	}
+	.right:hover:has( > div:last-of-type:hover){
+		flex: 0.9;
+	}
+
 	.right, .left {
 		display: flex;
 		flex-direction: column;
@@ -59,14 +72,18 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 	.left {
 		border-right: 1px solid $primary;
 		flex: 0.4;
+
 		div:first-of-type {
 			border-bottom: 1px solid $primary;
-			flex: 0.4;
+			flex: 0.4;	
 
+			svg {
+				animation: rotate3D 5s $ease-vnr infinite alternate;
+			}
+			
 			&:hover{
 				flex: 0.5;
 			}
-
 		}
 		div:last-of-type {
 			flex: 0.6;
@@ -75,15 +92,11 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 				flex: 0.8;
 			}
 			
-
 			svg {
 				transform: rotate(-4deg) scale(4.4) skew(27deg, -74deg);
+				animation: skeew 9s $ease-vnr infinite alternate;
 			}
 
-		}
-
-		&:hover {
-			flex: 0.6;
 		}
 
 	}
@@ -103,6 +116,7 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 
 			svg {
 				transform: rotate(90deg) scale(3.3) skew(45deg, -75deg);
+				animation: skeew2 8s $ease-vnr infinite alternate;
 			}
 		}
 		div:last-of-type {
@@ -112,6 +126,7 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 			}
 			svg {
 				transform: rotate(78.9deg) scale(4.0) skew(40deg, 10deg) translate(-20px,-20px);
+				animation: skeew3 6s $ease-vnr infinite alternate;
 			}
 		}
 	}
@@ -120,5 +135,54 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 		fill: $primary;
 	}
 
+	@keyframes rotate3D {
+		from {
+			transform: scale(1.0) rotate3d(0.5,0.8,0,0deg);
+		}
+		to {
+			transform: scale(1.2) rotate3d(0.5,0.8,0,360deg);
+		}
+	}
+
+	@keyframes skeew {
+		from {
+			transform: rotate(-4deg) scale(4.4) skew(27deg, -74deg);
+		}
+
+		to {
+			transform: rotate(-18deg) scale(2.4) skew(29deg, -4deg);
+		}
+	}
+
+
+	@keyframes skeew2 {
+		from {
+			transform: rotate(90deg) scale(3.3) skew(45deg, -75deg);
+		}
+
+		to {
+			transform: rotate(110deg) scale(5) skew(20deg, 50deg);
+		}
+	}
+
+	@keyframes skeew3 {
+		from {
+			transform: rotate(78.9deg) scale(4.0) skew(40deg, 10deg) translate(-20px,-20px);
+		}
+
+		to {
+			transform: rotate(90.9deg) scale(3.0) skew(10deg, 100deg) translate(-0px,-20px);
+		}
+	}
+
+	@keyframes wawing {
+		from {
+			flex: 0.4;
+		}
+
+		to {
+			flex: 0.6;
+		}
+	}
 
 </style>
