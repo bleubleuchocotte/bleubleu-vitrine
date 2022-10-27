b26005eea1981ccf645ecf995fcd44e60af7cfec
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-10-27 15:55:01 +0200

    remove project file

--- src/router.js
+++ src/router.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
-import ProjectPage from '@/pages/ProjectPage.vue';
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/projects', component: ProjectPage },
 ]
 
 export const router = createRouter({
