1a58c4756b6041362284b6cc70916f600cbe1762
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-11-07 21:34:10 +0100

    create lottie component + disable animation when not in viewport

---
+++ src/components/UI/LottieAnimation.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { Vue3Lottie } from 'vue3-lottie'
+import { defineProps, onMounted, ref } from 'vue';
+import 'vue3-lottie/dist/style.css'
+
+defineProps({
+	video: {
+		type: Object,
+		required: true
+	}
+})
+
+function createIntersectionObserver() {
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+  };
+
+  let callback = (entries) => {
+		entries[0].isIntersecting ? isPausing.value = false : isPausing.value = true;
+  };
+  let observer = new IntersectionObserver(callback, options);
+  observer.observe(container.value.$el);
+}
+
+onMounted(() => {
+	createIntersectionObserver();
+})
+
+const container = ref();
+const isPausing = ref(true)
+</script>
+
+<template>
+  <Vue3Lottie
+    ref="container"
+    :animation-data="video"
+    :pause-animation="isPausing"
+  />
+</template>
