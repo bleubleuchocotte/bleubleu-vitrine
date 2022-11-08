709f55b8bd8f8ef5f11d74358b79e65ab5078fe2
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-11-08 15:28:42 +0100

    remove css animation : optimization purpose for v1

--- src/components/UI/GridChelou.vue
+++ src/components/UI/GridChelou.vue
@@ -1,9 +1,6 @@
 <script setup>
 import IconBleubleu from "@/assets/icons/test.svg?inline";
-
-
 </script>
-
 
 <template>
   <div class="dynamic-grid">
@@ -28,9 +25,6 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 </template>
 
 <style scoped lang="scss">
-
-
-
 	.dynamic-grid {
 		display: flex;
 		height: 100%;
@@ -76,17 +70,9 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 		div:first-of-type {
 			border-bottom: 1px solid $primary;
 			flex: 0.4;	
-
-			svg {
-				animation: rotate3D 5s $ease-vnr infinite alternate;
-			}
 			
 			&:hover{
 				flex: 0.5;
-
-				svg {
-					animation-play-state: paused;
-				}
 			}
 		}
 		div:last-of-type {
@@ -94,23 +80,12 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 
 			&:hover{
 				flex: 0.8;
-				
-				svg {
-					animation-play-state: paused;
-				}
 			}
-			
-			svg {
-				transform: rotate(-4deg) scale(4.4) skew(27deg, -74deg);
-				animation: skeew 9s $ease-vnr infinite alternate;
-			}
-
 		}
 
 		&:hover {
 			flex: 0.6;
 		}
-
 	}
 	.right {
 		flex: 0.6;
@@ -125,29 +100,19 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 
 			&:hover{
 				flex: 1.2;
-				
-				svg {
-					animation-play-state: paused;
-				}
 			}
 
 			svg {
 				transform: rotate(90deg) scale(3.3) skew(45deg, -75deg);
-				animation: skeew2 8s $ease-vnr infinite alternate;
 			}
 		}
 		div:last-of-type {
 			flex: 0.3;
 			&:hover{
 				flex: 0.5;
-				
-				svg {
-					animation-play-state: paused;
-				}
 			}
 			svg {
 				transform: rotate(78.9deg) scale(4.0) skew(40deg, 10deg) translate(-20px,-20px);
-				animation: skeew3 6s $ease-vnr infinite alternate;
 			}
 		}
 	}
@@ -155,55 +120,4 @@ import IconBleubleu from "@/assets/icons/test.svg?inline";
 	svg {
 		fill: $primary;
 	}
-
-	@keyframes rotate3D {
-		from {
-			transform: scale(1.0) rotate3d(0.5,0.8,0,0deg);
-		}
-		to {
-			transform: scale(1.2) rotate3d(0.5,0.8,0,360deg);
-		}
-	}
-
-	@keyframes skeew {
-		from {
-			transform: rotate(-4deg) scale(4.4) skew(27deg, -74deg);
-		}
-
-		to {
-			transform: rotate(-18deg) scale(2.4) skew(29deg, -4deg);
-		}
-	}
-
-
-	@keyframes skeew2 {
-		from {
-			transform: rotate(90deg) scale(3.3) skew(45deg, -75deg);
-		}
-
-		to {
-			transform: rotate(110deg) scale(5) skew(20deg, 50deg);
-		}
-	}
-
-	@keyframes skeew3 {
-		from {
-			transform: rotate(78.9deg) scale(4.0) skew(40deg, 10deg) translate(-20px,-20px);
-		}
-
-		to {
-			transform: rotate(90.9deg) scale(3.0) skew(10deg, 100deg) translate(-0px,-20px);
-		}
-	}
-
-	@keyframes wawing {
-		from {
-			flex: 0.4;
-		}
-
-		to {
-			flex: 0.6;
-		}
-	}
-
 </style>
