609f34aa1e3b66b3b6e3588add388c6914a3f321
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-11-10 11:51:04 +0100

    create component

---
+++ src/components/UI/LineIcons.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import { defineProps } from "vue";
+
+defineProps({
+	icons: {
+		type: Array,
+		required: true
+	}
+})
+
+</script>
+
+<template>
+  <div>
+    <i
+      v-for="(icon, index) in icons"
+      :key="index"
+    >
+      {{ icon }}
+    </i>
+  </div>
+</template>
