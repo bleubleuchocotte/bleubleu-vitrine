78627be4dc906b87ba5eac60910caf43e61d5a84
Thomas <thomas.auffroy@gmail.com>
2022-12-07 11:34:25 +0100

    create component

---
+++ src/components/UI/SliderImages.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {defineProps} from "vue";
+
+defineProps({
+	image: {
+		type: Object,
+		required: true
+	}
+})
+</script>
+
+<template>
+  <div class="slider-images">
+    <img
+      :src="image.src"
+      :alt="image.alt"
+      width="640"
+      height="340"
+    >
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+	.slider-images {
+		position: absolute;
+		right: 0;
+		top: 0;
+
+		width: 40%;
+		height: 100%;
+	}
+
+</style>
