fb660e34281e13d458ab2744b107cdea4c692c1b
Thomas <thomas.auffroy@gmail.com>
2022-12-08 13:58:42 +0100

    split transform in two different option, scale and translate

--- src/components/UI/Cursor.vue
+++ src/components/UI/Cursor.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, onUnmounted, ref } from "vue";
+import {getValueWithBoundaries} from "@/composables/useMath.js"
 
 onMounted(() => {
 	if (window.matchMedia('(pointer: coarse)').matches) {
@@ -18,15 +19,28 @@ function updateCursor(arg){
 	cursor.value.style.zIndex = "100";
 	if (arg.target.localName === "a" || arg.target.localName === "button"){
 		cursor.value.style.filter = "none";
-		cursor.value.style.transform = `translate(${arg.clientX - 30}px, ${arg.clientY-30}px) scale(0.7)`;
+		cursor.value.style.scale = "0.5";
+		cursor.value.style.translate = `${arg.clientX - 30}px ${arg.clientY-30}px`;
 		cursor.value.style.borderWidth = "1px";
 		cursor.value.style.boxShadow = "none";
+		
 	} else {
 		const lastPosition = cursor.value.getBoundingClientRect() || {x: 0, y: 0};
+		
+		const posX = getValueWithBoundaries(lastPosition.x + (lastPosition.height / 2) - arg.clientX, -35, 35)
+		const posY = getValueWithBoundaries(lastPosition.y + (lastPosition.width / 2) - arg.clientY, -35, 35)
+
 		cursor.value.style.filter = "blur(15px)"
-		cursor.value.style.transform = `translate(${arg.clientX - 30}px, ${arg.clientY-30}px) scale(1)`;
+		cursor.value.style.scale = "1";
 		cursor.value.style.borderWidth = "0px";
-		cursor.value.style.boxShadow = `${lastPosition.x + 30 - arg.clientX}px ${lastPosition.y + 30 - arg.clientY}px 0px -5px, ${(lastPosition.x + 30 - arg.clientX)*1.5}px ${(lastPosition.y + 30 - arg.clientY)*1.5}px 0px -10px, ${(lastPosition.x + 30 - arg.clientX)*2}px ${(lastPosition.y + 30 - arg.clientY)*2}px 0px -15px, ${(lastPosition.x + 30 - arg.clientX)*2.5}px ${(lastPosition.y + 30 - arg.clientY)*2.5}px 0px -20px, ${(lastPosition.x + 30 - arg.clientX)*3}px ${(lastPosition.y + 30 - arg.clientY)*3}px 0px -25px`;
+		cursor.value.style.translate = `${arg.clientX - 30}px ${arg.clientY - 30}px`;
+		cursor.value.style.boxShadow = `
+			${posX}px ${posY}px 0px -5px, 
+			${(posX)*1.5}px ${(posY)*1.5}px 0px -10px, 
+			${(posX)*2}px ${(posY)*2}px 0px -15px, 
+			${(posX)*2.5}px ${(posY)*2.5}px 0px -20px, 
+			${(posX)*3}px ${(posY)*3}px 0px -25px
+		`;
 	}
 }
 </script>
@@ -50,12 +64,11 @@ function updateCursor(arg){
 
 	pointer-events: none;
 
-
 	background-color: $primary;
   border-radius: 50%;
 	border: 1px solid $secondary;
 
-  transition: filter 0.3s ease;
+  transition: filter 0.3s ease, scale 0.3s ease;
 }
 
 </style>
