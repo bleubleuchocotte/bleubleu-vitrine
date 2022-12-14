717139986c22f5abe3e0cf2a669f9d8d9178edda
Thomas <thomas.auffroy@gmail.com>
2022-12-14 11:54:18 +0100

    sort project by dates

--- src/components/sections/homepage/content/ProjectsContent.vue
+++ src/components/sections/homepage/content/ProjectsContent.vue
@@ -1,21 +1,38 @@
 <script setup>
-import { defineProps, ref } from 'vue';
+import { defineProps, ref, onMounted } from 'vue';
 import { useMedia } from "@/composables/useMedia.js"
 import ProjectRepresentation from '@/components/UI/ProjectRepresentation.vue';
 
-defineProps({
+const props = defineProps({
 	fields: {
 		type: Array,
 		required: true,
 	}
 })
 
+onMounted(() => {
+  bufferDatas.value = props.fields;
+  bufferDatas.value.sort(compareFn)
+})
+
+function compareFn(a, b) {
+  if (a.data.date > b.data.date) {
+    return -1;
+  }
+  if (a.data.date < b.data.date) {
+    return 1;
+  }
+  return 0;
+}
+
+const bufferDatas = ref([])
+
 const hasMouse = ref(useMedia('(pointer: fine)'));
 </script>
 
 <template>
   <ProjectRepresentation
-    v-for="(project, index) in fields"
+    v-for="(project, index) in bufferDatas"
     :key="index"
     :project="project"
     :active-mouse-effect="hasMouse"
